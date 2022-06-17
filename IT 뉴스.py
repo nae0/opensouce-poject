12299c1fd4f2592991da15b0aaa139fb342cc978
kangmj06 <92535725+kangmj06@users.noreply.github.com>
2022-06-17 22:26:04 +0900

    Add files via upload

--- IT 뉴스.py
+++ IT 뉴스.py
@@ -1,25 +1,7 @@
-from bs4 import BeautifulSoup
-import requests 
-import re
-from selenium import webdriver
-import time
-# BeautifulSoup 객체만들기
-def create_soup(url):
-    headers = {
-        'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36')
-
-}
-
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    return soup
- 
 def scrape_it_news():
-    print("[[ IT 뉴스 ]]")
-    url="https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230"
-    soup = create_soup(url)
-    news_list = soup.find("ul",attrs={"class":"type06_headline"}).find_all("li", limit=3)
+    print("[[ 오늘의 IT 뉴스 ]]\n")
+    url=""
+    news_list = find("ul",attrs={}).find_all("li", limit=5)
     for index, news in enumerate(news_list):
         a_idx=0
         img=news.find("img")
@@ -29,11 +11,5 @@ def scrape_it_news():
         title=news.find_all("a")[a_idx].get_text().strip()
         link=news.find_all("a")[a_idx]["href"]
         print("{}. {}".format(index, title))
-        print("   (링크 : {})".format(link))
+        print("   (링크 : {})".format(link)) 
     print()
-
-
-
-if __name__=='__main__':
-    scrape_it_news()
- 
